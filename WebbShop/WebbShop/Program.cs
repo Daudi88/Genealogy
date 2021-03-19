ae366d7b6c99fabaad095776e4d48d8194cf1d68
David Ström <david.strom88@gmail.com>
2021-03-19 09:27:15 +0100

    .

---
+++ WebbShop/WebbShop/Program.cs
@@ -0,0 +1,16 @@
+﻿using WebbShop.Helpers;
+
+namespace WebbShop
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// First method to be called.
+        /// </summary>
+        private static void Main()
+        {
+            Seeder.Seed();
+            Menu.MainMenu();
+        }
+    }
+}
